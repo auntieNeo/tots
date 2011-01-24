6b6f7674394b8f17a3cbbf399b01dda0d664934d
auntieNeo <auntieNeo@gmail.com>
2011-01-23 20:26:47 -0700

    Added the lua level script and the Doxygen file.

---
+++ level01.lua
@@ -0,0 +1,55 @@
+--[[ Lua  functions needed to spawn an entity:
+--    [ ] addSprite
+--    [ ] getSprite
+--    [ ] addMovement
+--    [ ] getMovement
+--    [ ] addBehavior
+--    [ ] getBehavior
+--    [ ] addEntity
+--    [ ] copyEntity
+--    [ ] spawnEntity
+--
+--    [ ] Sprite
+--
+--    [ ] Path
+--    [ ] Path_lineTo
+--    [ ] Path_quadTo
+--    [ ] Path_cubicTo
+--
+--    [ ] Behavior
+--    [ ] Behavior_setMovement
+--
+--    [ ] Entity
+--]]
+function foobar()
+  blah = blah + 1
+  spawnEntity(blah, 100 - (blah % 15) * 25, copyEntity("enemy"))
+end
+
+function initLevel()
+  addSprite(Sprite("player", "sprites/entityTest-32x32.png", 32, 32,
+      3,  -- number of states
+      0, 2, 0,  -- number of frames per state
+      0, 42, 0,  -- frames per second
+      2))  -- frames per row
+
+  -- Enemy
+  addSprite(Sprite("enemy", "sprites/entityTest-32x32.png", 32, 32,
+      3,  -- number of states
+      0, 2, 0,  -- number of frames per state
+      0, 42, 0,  -- frames per second
+      2))  -- frames per row
+  path = Path("enemy", 0.5)
+  Path_quadTo(path, 150, 0, 150, 150, 0.5)
+  Path_quadTo(path, 150, 350, 0, 350, 2)
+  addMovement(path)
+  behavior = Behavior("enemy", 3)
+  Behavior_setMovement(behavior, 1, getMovement("enemy"))
+  addBehavior(behavior)
+  addEntity(Entity("enemy", 1, getSprite("enemy"), getBehavior("enemy")))
+  
+  blah = 10
+  for i=1,500 do
+    addTimedEvent(level, 3+(i * 0.005), "foobar")
+  end
+end
