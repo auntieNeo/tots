ff775510554f561cae840df5034f559f505d8042
Jonathan Glines <auntieNeo@gmail.com>
2014-06-29 16:26:43 -0600

    Moved old files. Started working on new SDL2 implementation.

--- src/main.cpp
+++ src/main.cpp
@@ -1,75 +1,13 @@
-// Typing of the Stars
-#include <allegro5/allegro.h>
-#include <allegro5/allegro_image.h>
-#include <allegro5/allegro_primitives.h>
-#include <allegro5/allegro_ttf.h>
-
-#include "common.h"
-#include "level.h"
-
-#include<cassert>
-
-#include <iostream>
-using namespace std;
-
-/*!
- * \namespace Tots
- * The namespace used by Typing of the Stars.
- */
+#include <SDL2/SDL.h>
 
 int main(int argc, char **argv)
 {
-  if(al_init() != true)
-    return 1;
-
-  if(al_install_keyboard() != true)
-    return 1;
-
-  if(al_init_primitives_addon() != true)
-    return 1;
-
-  if(al_init_image_addon() != true)
-    return 1;
-
-  ALLEGRO_DISPLAY *display = al_create_display(500, 768);
-
-  assert(argc == 2);
-  Tots::Level *level;
-  level = new Tots::Level(argv[1]);
-
-  ALLEGRO_EVENT_QUEUE *queue = al_create_event_queue();
-
-  ALLEGRO_TIMER *timer = al_create_timer(1.0 / FRAMES_PER_SECOND);
-  al_register_event_source(queue, al_get_timer_event_source(timer));
-  al_start_timer(timer);
-
-  ALLEGRO_EVENT event;
-
-  bool needDraw = false;
-  while(true)
+  if(SDL_Init((SDL_INIT_VIDEO | SDL_INIT_TIMER) != 0))
   {
-    al_wait_for_event(queue, &event);
-
-    // TODO: handle input events
-
-    if(event.type == ALLEGRO_EVENT_TIMER)
-    {
-      level->tick();
-      needDraw = true;
-    }
-    if(needDraw && al_event_queue_is_empty(queue))
-    {
-      level->draw();
-      al_flip_display();
-      needDraw = false;
-    }
+    fprintf(stderr,
+            "Unable to initialize SDL: %s\n",
+            SDL_GetError());
+    return 1;
   }
-
-  al_destroy_timer(timer);
-
-  al_destroy_event_queue(queue);
-
-  al_destroy_display(display);
-
   return 0;
 }
